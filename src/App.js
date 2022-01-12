5ee47645f37d6161379c36cb3494843e7bfebd97
emei6793 <emei6793@richmondhillhs.org>
2022-01-12 16:51:19 +0000

    NYSED final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+import SchoolCard from './components/SchoolCard.js';
+import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
@@ -11,7 +11,15 @@ function App() {
       <Navbar/>
       <div className="container">
         <div className="row">
+        {data.map (school => (
+            <SchoolCard
+            name = {school.ENTITY_NAME}
+            year = {school.YEAR}
+            black = {school.NUM_BLACK}
+            asian = {school.NUM_ASIAN}
+            white = {school.NUM_WHITE}/>))}
 
+            
         </div>
       </div>
     </div>
--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.name}</h5>
         <div className="card-body">
           <h5 className="card-title">
-            <span className="badge">2017-18</span>
+            <span className="badge">{props.year}</span>
             <span className="badge">2016-17</span>
             <span className="badge">Archive</span>
           </h5>
-          <span className="badge badge-header">Student Data</span>
+          <span className="badge badge-header">Ethnicity</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
-            <li className="list-group-item">3-8 Math Assessment Data</li>
+            <li className="list-group-item">Number of Black Students: {props.black}</li>
+            <li className="list-group-item">Number of Asian Students:  {props.asian}</li>
+            <li className="list-group-item">Number of White Students:  {props.white}</li>
           </ul>
           <span className="badge badge-header">School Data</span>
           <ul className="list-group">
